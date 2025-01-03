a272198ce47c4c13d605e67e8a7edc9267670d4b
Jarosław Jelinek <jaroo2204@gmail.com>
2025-01-03 16:32:51 +0100

    Dodane kategorie do Bazy

---
+++ DachyWeb/Data/ApplicationDbContext.cs
@@ -0,0 +1,25 @@
+﻿using DachyWeb.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace DachyWeb.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+            
+        }
+
+        public DbSet<Category> Categories { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Category>().HasData(
+                new Category { CategoryId = 1, Name = "Pokrycia stalowe", DisplayOrder = 1 },
+                new Category { CategoryId = 2, Name = "Dachówki ceramiczne", DisplayOrder = 2 },
+                new Category { CategoryId = 3, Name = "Dachówki cementowe", DisplayOrder = 3 },
+                new Category { CategoryId = 4, Name = "Systemy rynnowe", DisplayOrder = 4 },
+                new Category { CategoryId = 5, Name = "Akcesoria", DisplayOrder = 5 }
+                );
+        }
+    }
+}
