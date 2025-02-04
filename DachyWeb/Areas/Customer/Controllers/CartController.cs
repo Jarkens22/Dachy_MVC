e8a6c58a20017600b332fd5b7c3e958a31184fb2
Jarosław Jelinek <jaroo2204@gmail.com>
2025-02-04 14:35:30 +0100

    Dodano potwierdzenie złożenia zamówienia

--- DachyWeb/Areas/Customer/Controllers/CartController.cs
+++ DachyWeb/Areas/Customer/Controllers/CartController.cs
@@ -1,6 +1,7 @@
 ﻿using Dachy.DataAccess.Repository.IRepository;
 using Dachy.Models;
 using Dachy.Models.ViewModels;
+using Dachy.Utility;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using System.Security.Claims;
@@ -12,6 +13,7 @@ namespace DachyWeb.Areas.Customer.Controllers
     public class CartController : Controller
     {
         private readonly IUnitOfWork _unitOfWork;
+        [BindProperty]
         public ShoppingCartViewModel ShoppingCartViewModel { get; set; }
         public CartController(IUnitOfWork unitOfWork)
         {
@@ -69,6 +71,72 @@ namespace DachyWeb.Areas.Customer.Controllers
             }
             return View(ShoppingCartViewModel);
         }
+
+        [HttpPost]
+        [ActionName("Summary")]
+        public IActionResult SummaryPOST()
+        {
+            var claimsIdentity = (ClaimsIdentity)User.Identity;
+            var userId = claimsIdentity.FindFirst(ClaimTypes.NameIdentifier).Value;
+
+            ShoppingCartViewModel.ShoppingCartList = _unitOfWork.ShoppingCart.GetAll(u => u.ApplicationUserId == userId,
+                includeProperties: "Product");
+
+            ShoppingCartViewModel.OrderHeader.OrderDate = System.DateTime.Now;
+            ShoppingCartViewModel.OrderHeader.ApplicationUserId = userId;
+
+            ApplicationUser applicationUser= _unitOfWork.ApplicationUser.Get(u => u.Id == userId);
+
+            foreach (var cart in ShoppingCartViewModel.ShoppingCartList)
+            {
+                cart.Price = GetPriceBasedOnQuantity(cart);
+                ShoppingCartViewModel.OrderHeader.OrderTotal += (cart.Price * cart.Count);
+            }
+
+            if (applicationUser.CompanyId.GetValueOrDefault() == 0)
+            {
+                //jest to zwykły użytkownik
+                ShoppingCartViewModel.OrderHeader.PaymentStatus = SD.PaymentStatusPending;
+                ShoppingCartViewModel.OrderHeader.OrderStatus = SD.StatusPending;
+            }
+            else
+            {
+                //to użytkownik firmowy i może zapłacić po złożeniu zamówienia
+                ShoppingCartViewModel.OrderHeader.PaymentStatus = SD.PaymentStatusDelayedPayment;
+                ShoppingCartViewModel.OrderHeader.OrderStatus = SD.StatusApproved;
+            }
+
+            _unitOfWork.OrderHeader.Add(ShoppingCartViewModel.OrderHeader);
+            _unitOfWork.Save();
+
+            foreach (var cart in ShoppingCartViewModel.ShoppingCartList)
+            {
+                OrderDetails orderDetails = new()
+                {
+                    ProductId = cart.ProductId,
+                    OrderHeaderId = ShoppingCartViewModel.OrderHeader.Id,
+                    Price = cart.Price,
+                    Count = cart.Count
+                };
+
+                _unitOfWork.OrderDetails.Add(orderDetails);
+                _unitOfWork.Save();
+            }
+
+            if (applicationUser.CompanyId.GetValueOrDefault() == 0)
+            {
+                //jest to zwykły użytkownik i musimy otrzymać płatność
+
+            }
+
+            return RedirectToAction(nameof(OrderConfirmation), new {id=ShoppingCartViewModel.OrderHeader.Id});
+        }
+
+        public IActionResult OrderConfirmation(int id) 
+        {
+            return View(id);
+        }
+
 
         public IActionResult Plus(int cartId)
         {
