81f4ff220485a7d406a9b124b377b3c579405b52
Jarosław Jelinek <jaroo2204@gmail.com>
2025-02-06 21:26:52 +0100

    lista zamowien

--- DachyWeb/wwwroot/js/product.js
+++ DachyWeb/wwwroot/js/product.js
@@ -8,9 +8,9 @@ function loadDataTable() {
     dataTable = $('#tblData').DataTable({
         "ajax": { url: '/admin/product/getall'},
         "columns": [
-        { data: 'name', "width": "15%" },
-        { data: 'producent', "width": "5%" },
-        { data: 'listPrice', "width": "5%" },
+            { data: 'name', "width": "15%" },
+            { data: 'producent', "width": "5%" },
+            { data: 'listPrice', "width": "5%" },
             { data: 'category.name', "width": "10%" },
             {
                 data: 'productId',
